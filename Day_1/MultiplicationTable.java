faf5441a5431cef9e6528e28765f6a5d9fff0a33
divyanshidhaka <divyanshidhaka33@gmail.com>
2026-05-28 17:39:11 +0530

    Create MultiplicationTable.java
    Here is the Solution of Question number 2 of Day_1

---
+++ Day_1/MultiplicationTable.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public MultiplicationTable {
+    public static void main (String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        for(int i = 1; i<=10;i++){
+            Syst.out.println(num+" * = "+num*i);
+        }
+    }
+
+}
